040a1daef7ed7e055d08725cc1ea788cf9752cb9
Bakili Shaina <shaina3322@hotmail.com>
2022-02-14 22:25:37 +0100

    card movie and TopMivie page

--- src/app/all-movies/all-movies.component.ts
+++ src/app/all-movies/all-movies.component.ts
@@ -12,10 +12,7 @@ export class AllMoviesComponent implements OnInit {
   constructor(private api: APIService) {
     this.api.getMovieData("").subscribe(datas => {
       this.data = Array(datas)
-      console.log(this.data.valueOf());
       this.data = this.data[0]["results"];
-      //console.log(this.data.valueOf());
-      //console.log(this.data[0]["results"]);
     })
   }
   ngOnInit(): void {
