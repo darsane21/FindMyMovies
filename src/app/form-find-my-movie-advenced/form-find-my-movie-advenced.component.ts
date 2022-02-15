8b61c9cd413e2f4462041ea39b85f990ea90020d
soudarsane <tsoudar21@gmail.com>
2022-02-15 11:09:08 +0100

    prends en compte la langue

--- src/app/form-find-my-movie-advenced/form-find-my-movie-advenced.component.ts
+++ src/app/form-find-my-movie-advenced/form-find-my-movie-advenced.component.ts
@@ -12,19 +12,21 @@ export class FormFindMyMovieAdvencedComponent implements OnInit {
   SignupForm: any;
   data: any;
   submitted = false;
+  novalues = false;
   name: any;
   year: any;
-
+  language:any;
+  link:any;
   constructor(private api: APIService) {
   }
 
   ngOnInit() {
+    this.novalues = false;
     this.SignupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl()
-      }),
-      year: new FormGroup({
-        yearNumber: new FormControl()
+        username: new FormControl(),
+        yearNumber: new FormControl(),
+        language : new FormControl()
       })
     });
   }
@@ -32,12 +34,20 @@ export class FormFindMyMovieAdvencedComponent implements OnInit {
   onSubmit() {
     const btn = document.getElementById("main") as HTMLInputElement;
     btn.classList.toggle("h-auto");
-    this.year = this.SignupForm["controls"]["year"]["value"]["yearNumber"];
+    this.year = this.SignupForm["controls"]["userData"]["value"]["yearNumber"];
     this.name = this.SignupForm["controls"]["userData"]["value"]["username"];
+    this.language = this.SignupForm["controls"]["userData"]["value"]["language"];
+
     this.submitted = true;
-    this.api.getMovieData('https://api.themoviedb.org/3/search/movie?api_key=e6171b13d4159aa39793cc0b447bbb93&primary_release_year=' + this.year + '&query=' + this.name + '&sort_by=vote_average.desc').subscribe(datas => {
-      this.data = Array(datas)
-      this.data = this.data[0]["results"];
+    this.novalues = false;
+    this.link='https://api.tmdb.org/3/search/movie?api_key=e6171b13d4159aa39793cc0b447bbb93&query='+this.name+'&year='+this.year+'&language='+this.language
+
+    this.api.getMovieData(this.link).subscribe(datas => {
+    this.data = Array(datas)
+    this.data = this.data[0]["results"];
+    console.log(this.data);
+    if(this.data.length==0)
+      this.novalues = true;
     })
   }
 }
